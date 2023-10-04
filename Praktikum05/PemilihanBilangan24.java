ee82131f9b54df227ac501aaa0e2787f3c32ad2e
Nova Diana <nopadr12@gmail.com>
2023-10-04 11:23:50 +0700

    Praktikum05_ternaryoperator

--- Praktikum05/PemilihanBilangan24.java
+++ Praktikum05/PemilihanBilangan24.java
@@ -7,14 +7,8 @@ public class PemilihanBilangan24 {
     System.out.print("Masukkan sebuah angka: ");
     int angka = sc.nextInt();
 
-    if (angka % 2 == 0)
-    {
-        System.out.println("Angka " + angka + " termasuk bilangan genap");
-    }
-    else
-    {
-        System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-    }
-    
+    String result = (angka % 2 == 0) ? "termasuk bilangan genap" : "termasuk bilangan ganjil";
+    System.out.println(result);
+
     }
 }
