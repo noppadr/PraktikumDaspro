6a9d83359d420d13d4372a34e4759c1c93a11c98
Nova Diana <nopadr12@gmail.com>
2023-10-04 14:10:22 +0700

    PemilihanHari24.java

---
+++ Praktikum05/PemilihanOperator24.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PemilihanOperator24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double  angka1, angka2, hasil = 0;
+        char operator;
+
+        System.out.print("Masukkan angka 1: ");
+        angka1 = sc.nextDouble();
+        
+    }
+}
