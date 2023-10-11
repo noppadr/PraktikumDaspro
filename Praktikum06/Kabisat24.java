7de47946b5469417de92390e3ad8230fdb82453b
Nova Diana <nopadr12@gmail.com>
2023-10-11 13:22:04 +0700

    Modifikadi KabisatNested tanpa operator logika

--- Praktikum06/Kabisat24.java
+++ Praktikum06/Kabisat24.java
@@ -8,12 +8,16 @@ public class Kabisat24 {
         System.out.println("Masukkan Tahun Kabisat: ");
         tahun = input.nextInt();
 
-        if ((tahun %4 == 0) && (tahun %100 !=0) || (tahun %4 == 0) && (tahun %100 !=0)) {
-            System.out.println("Tahun Kabisat");
-        } else {
+        if ((tahun %400) ==0 ){
+            System.out.print("Tahun Kabisat");
+        } else if ((tahun % 100) == 0) {
             System.out.println("Bukan Tahun Kabisat");
+        } else if ((tahun %4) == 0) {
+            System.out.println("Tahun Kabisat");
+        } else
+        System.out.println("Bukan Tahun Kabisat");
         }
         }
 
-    }
+    
 
