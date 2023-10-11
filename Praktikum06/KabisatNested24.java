4c09ae14459f62757a67f9b8e5817bd4ad39a49d
Nova Diana <nopadr12@gmail.com>
2023-10-11 11:07:40 +0700

    KabisatNested

---
+++ Praktikum06/KabisatNested24.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class KabisatNested24 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int tahun;
+
+        System.out.println("Masukkan Tahun : ");
+        tahun = input.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0)
+                System.out.println("Tahun Kabisat");
+        } else
+            System.out.println("Bukan Tahun Kabisat");
+    }
+}
