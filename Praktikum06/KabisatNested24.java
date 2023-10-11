862ac6d1a53942b92aa3c91d38d18f3d5f31f743
Nova Diana <nopadr12@gmail.com>
2023-10-11 11:22:25 +0700

    Kabisat Nested Modifikasi

--- Praktikum06/KabisatNested24.java
+++ Praktikum06/KabisatNested24.java
@@ -9,7 +9,7 @@ public class KabisatNested24 {
         tahun = input.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
+            if ((tahun % 1000) != 0)
                 System.out.println("Tahun Kabisat");
         } else
             System.out.println("Bukan Tahun Kabisat");
