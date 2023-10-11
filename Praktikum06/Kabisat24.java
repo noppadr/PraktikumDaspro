d91d0fb9ffe8f59a75267f52a8135916cfd5344e
Nova Diana <nopadr12@gmail.com>
2023-10-11 12:04:35 +0700

    without nested if, with logical expression

---
+++ Praktikum06/Kabisat24.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Kabisat24 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int tahun;
+
+        System.out.println("Masukkan Tahun Kabisat: ");
+        tahun = input.nextInt();
+
+        if ((tahun %4 == 0) && (tahun %100 !=0) || (tahun %4 == 0) && (tahun %100 !=0)) {
+            System.out.println("Tahun Kabisat");
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+        }
+
+    }
+
