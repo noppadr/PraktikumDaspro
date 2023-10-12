05b78ea1239ed32976e843473f6805e572dbee2b
Nova Diana <nopadr12@gmail.com>
2023-10-12 23:26:03 +0700

    pertanyaan no 7 percobaan 1

--- Praktikum06/KabisatNested24.java
+++ Praktikum06/KabisatNested24.java
@@ -8,17 +8,14 @@ public class KabisatNested24 {
         System.out.print("Masukkan Tahun : ");
         tahun = input.nextInt();
 
-        if (tahun %4 == 0) {
-            if (tahun % 100 !=0) {
-                System.out.println("Tahun Kabisat");
-            } else {
-                System.out.println("Bukan Tahun Kabisat");
-            }
-        }
-        else {
+        if ((tahun %400) == 0) {
+            System.out.println("Tahun Kabisat");
+        } else if ((tahun % 100) == 0) {
             System.out.println("Bukan Tahun Kabisat");
-        }
-            
+        } else if ((tahun %4) == 0) {
+            System.out.println("Tahun Kabisat");
+        } else
+        System.out.println("Bukan Tahun Kabisat");
         }    
     }
 
