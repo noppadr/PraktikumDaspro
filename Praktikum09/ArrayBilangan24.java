d44a6e0889d41384f34dcf6a7f9efa96dc4bf9fc
Nova Diana <nopadr12@gmail.com>
2023-11-01 13:59:28 +0700

    modif array bilangan

--- Praktikum09/ArrayBilangan24.java
+++ Praktikum09/ArrayBilangan24.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class ArrayBilangan24 {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
-        int[] bil = new int[4];
-        for ( int i = 0; i <=4; i++) {
+        int[] bil = {5, 13, -7, 17};
+            
+        for (int i = 0; i <= 4; i++){
             System.out.println(bil[i]);
+
         }
     }
 }
