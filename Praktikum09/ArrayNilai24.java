f33dc5b73e45c55226f4ce2741efdfab170f06ad
Nova Diana <nopadr12@gmail.com>
2023-11-01 14:55:55 +0700

    linear search

--- Praktikum09/ArrayNilai24.java
+++ Praktikum09/ArrayNilai24.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class ArrayNilai24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+        int nilaiTertinggi = 0;
+        int nilaiTerendah = 100;
         int[]  nilaiAkhir = new int[10];
+        double totalLulus = 0, totalTdkLulus = 0, rataLulus, rataTdkLulus;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir mahasiswa ke-" + (i+1) + ": ");
@@ -12,10 +16,17 @@ public class ArrayNilai24 {
 
         for (int i = 0; i < 10; i++) {
             if (nilaiAkhir[i] > 70) {
-                System.out.println("Mahasiswa ke-" + (i+1) + " lulus");
-            } 
-            else if (nilaiAkhir[i] < 70)
-                System.out.println("Mahasiswa ke-" + (i+1) + " tidak lulus");
+                totalLulus = totalLulus + nilaiAkhir[i];
+                lulus++;
+            } else {
+                totalLulus = totalTdkLulus + nilaiAkhir[i];
+                tidakLulus++;
             }
         }
+
+        rataLulus = totalLulus / lulus;
+        rataTdkLulus = totalTdkLulus / tidakLulus;
+        System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        System.out.println("Rata-rata tidak lulus " + rataTdkLulus);
     }
+}
--- Praktikum09/LinearSearch24.java
+++ Praktikum09/LinearSearch24.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class LinearSearch24 {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+
+        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
+        int key = 20;
+        int hasil = 0;
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key) {
+                hasil = i;
+                break;
+            }
+        }
+
+        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+    }
 }
