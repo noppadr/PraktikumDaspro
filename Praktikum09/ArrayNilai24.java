e52d22513577105fc1ef41ce47394029ea0f1cae
Nova Diana <nopadr12@gmail.com>
2023-11-01 14:26:46 +0700

    modif status kelulusan

--- Praktikum09/ArrayNilai24.java
+++ Praktikum09/ArrayNilai24.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class ArrayNilai24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        int[]  nilaiAkhir = new int[11];
+        int[]  nilaiAkhir = new int[10];
 
-        for (int i = 1; i < 11; i++) {
-            System.out.print("Masukkan nilai akhir mahasiswa ke-" + (i) + ": ");
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir mahasiswa ke-" + (i+1) + ": ");
             nilaiAkhir[i] = sc.nextInt();
         }
-        for (int i = 1; i < 11; i++)
-            System.out.println("Nilai akhir mahasiswa ke-" + (i) + " adalah " + nilaiAkhir[i] );
+
+        for (int i = 0; i < 10; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + (i+1) + " lulus");
+            } 
+            else if (nilaiAkhir[i] < 70)
+                System.out.println("Mahasiswa ke-" + (i+1) + " tidak lulus");
+            }
+        }
     }
-}
