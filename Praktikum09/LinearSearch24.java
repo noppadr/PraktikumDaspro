618037cf0cff45860e784dbcf0c9f2770e436a4f
Nova Diana <nopadr12@gmail.com>
2023-11-03 12:41:53 +0700

    modifikasi pesan key

--- Praktikum09/LinearSearch24.java
+++ Praktikum09/LinearSearch24.java
@@ -4,17 +4,29 @@ public class LinearSearch24 {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
 
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        int[] arrayInt = new int[8];
+        int key = 0, hasil = 0, array;
+
+        System.out.print("Masukkan jumlah elemen array: ");
+        array = input.nextInt();
+
 
         for (int i = 0; i < arrayInt.length; i++) {
-            if (arrayInt[i] == key) {
-                hasil = i;
-                break;
-            }
-        }
+            System.out.print("Masukkan elemen array yang ke- " +i+ " : ");
+                arrayInt[i] = input.nextInt();
 
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+            } System.out.print("Masukkan key yang ingin dicari: ");
+            key = input.nextInt();
+            for (int i =0; i < arrayInt.length; i++){
+                if (arrayInt[i] == key){
+                    hasil =i;
+                    System.out.print("key dalam array pada posisi indeks ke- "+hasil);
+                    break;
+                } else {
+                    System.out.println("key tidak ditemukan");
+                    break;
+            }
+            
+        }
     }
 }
