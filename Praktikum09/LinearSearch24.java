076f651229f976c4161371eddebb6943082b47ca
Nova Diana <nopadr12@gmail.com>
2023-11-08 11:01:28 +0700

    modif menampilkan semua posisi indeks

--- Praktikum09/LinearSearch24.java
+++ Praktikum09/LinearSearch24.java
@@ -4,29 +4,33 @@ public class LinearSearch24 {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
 
-        int[] arrayInt = new int[8];
-        int key = 0, hasil = 0, array;
+        int key = 0;
+        int hasil = -1;
 
         System.out.print("Masukkan jumlah elemen array: ");
-        array = input.nextInt();
-
+        int elemen = input.nextInt();
+        int[] arrayInt = new int[elemen];
 
         for (int i = 0; i < arrayInt.length; i++) {
-            System.out.print("Masukkan elemen array yang ke- " +i+ " : ");
-                arrayInt[i] = input.nextInt();
-
-            } System.out.print("Masukkan key yang ingin dicari: ");
-            key = input.nextInt();
-            for (int i =0; i < arrayInt.length; i++){
-                if (arrayInt[i] == key){
-                    hasil =i;
-                    System.out.print("key dalam array pada posisi indeks ke- "+hasil);
-                    break;
-                } else {
-                    System.out.println("key tidak ditemukan");
-                    break;
-            }
-            
+            System.out.print("Masukkan elemen array ke-" + i + ": ");
+            arrayInt[i] = input.nextInt();        
         }
+
+        System.out.println("Masukkan key yang ingin dicari: ");
+        key = input.nextInt();
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key) {
+                hasil = i;
+
+                System.out.println("key ada dalam array pada posisi indeks ke-" + hasil);
+            }
+        }
+
+        if (hasil == -1) {
+            System.out.println("Key tidak ditemukan dalam array");
+        }
+        
     }
 }
+    
