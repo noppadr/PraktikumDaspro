48e2092c7668e901af3651d905d16e08c4b16dcd
Nova Diana <nopadr12@gmail.com>
2023-11-15 12:52:28 +0700

    bintang persegi

---
+++ Praktikum11/Square24.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Square24 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah * : ");
+        int n = input.nextInt();
+
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    System.out.print("*");
+                }
+
+                System.out.println();
+            }
+        }
+    }
+
