8dec63671b6e0c5c01d8032aec67fcb251db75b0
Nova Diana <nopadr12@gmail.com>
2023-11-15 13:09:30 +0700

    Bintang Persegi

--- Praktikum11/Square24.java
+++ Praktikum11/Square24.java
@@ -7,9 +7,12 @@ public class Square24 {
         System.out.print("Masukkan jumlah * : ");
         int n = input.nextInt();
 
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    System.out.print("*");
+        for (int i = 1; i <= n; i++) {
+            System.out.print("*");
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
                 }
 
                 System.out.println();
