821f43b9c076ef94d579c0b5a90eb1f4ac773109
Nova Diana <nopadr12@gmail.com>
2023-11-15 15:25:39 +0700

    modif for each, rata suhu, jml kota

--- Praktikum11/NestedLoop24.java
+++ Praktikum11/NestedLoop24.java
@@ -4,7 +4,10 @@ public class NestedLoop24 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double[][] suhu = new double[2][4];
+        System.out.print("Masukkan Jumlah Kota: ");
+        int jumlahKota = input.nextInt();
+        double[][] suhu = new double[jumlahKota][4];
+
         for (int i = 0; i < suhu.length; i++) {
             System.out.println("KOTA KE-" + (i + 1));
 
@@ -16,10 +19,18 @@ public class NestedLoop24 {
         for (int i = 0; i < suhu.length; i++) {
             System.out.print("KOTA KE-" + (i + 1) + ": ");
 
-            for (int j = 0; j < suhu[i].length; j++) {
-                System.out.print(suhu[i][j] + " ");
+            for (double temperature : suhu [i]) {
+                System.out.print(temperature + " ");
             }
             System.out.println();
+        }
+        for (int i = 0; i < suhu.length; i++) {
+            double totalSuhu = 0;
+            for (int j = 0; j < suhu[i].length; j++) {
+                totalSuhu += suhu[i][j];
+            }
+            double rataRata = totalSuhu / suhu[i].length;
+            System.out.println("Rata-rata suhu kota " + (i +1) + ": " + rataRata);
         }
     }
 }
