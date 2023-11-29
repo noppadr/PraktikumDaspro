ab1a8e84c546a8f397c3f6af1e8079de2dc98ee6
Nova Diana <nopadr12@gmail.com>
2023-11-29 11:58:49 +0700

    modif function +

--- Praktikum13/School24.java
+++ Praktikum13/School24.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class School24 {
     public static void display(String[] students) {
         Scanner sc = new Scanner(System.in);
-        System.out.printf("=========================%n");
+        System.out.printf("+++++++++++++++++++++++++%n");
         System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
-        System.out.printf("=========================%n");
+        System.out.printf("+++++++++++++++++++++++++%n");
         
         for (int i = 0; i < students.length; i++) {
             System.out.printf("| %-3d | %-15s |%n", (i + 1), students[i]);
         }
 
-        System.out.printf("=========================%n%n");
+        System.out.printf("+++++++++++++++++++++++++%n%n");
     }
     public static void main(String[] args) {
         String[] students1A = { "Anne", "Aldrian", "Ali", "Arul", "Ashley" };
