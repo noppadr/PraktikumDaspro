20c3a3e9492c6360af34f7dd02719fdb99984aed
Nova Diana <nopadr12@gmail.com>
2023-11-29 10:59:01 +0700

    percobaan 1

---
+++ Praktikum13/Welcome24.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Welcome24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        displayWelcomeMessage();
+    }
+    public static void displayWelcomeMessage(){
+        System.out.println("Selamat datang di Politeknik Negeri Malang");
+        }
+}
