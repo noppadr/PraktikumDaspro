5166089599932067999a9183833703fe4c9bb808
Nova Diana <nopadr12@gmail.com>
2023-11-29 11:53:56 +0700

    modif function =

--- Praktikum13/School24.java
+++ Praktikum13/School24.java
@@ -1,57 +1,28 @@
 import java.util.Scanner;
 
 public class School24 {
-    public static void main(String[] args) {
+    public static void display(String[] students) {
         Scanner sc = new Scanner(System.in);
+        System.out.printf("=========================%n");
+        System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
+        System.out.printf("=========================%n");
+        
+        for (int i = 0; i < students.length; i++) {
+            System.out.printf("| %-3d | %-15s |%n", (i + 1), students[i]);
+        }
 
+        System.out.printf("=========================%n%n");
+    }
+    public static void main(String[] args) {
         String[] students1A = { "Anne", "Aldrian", "Ali", "Arul", "Ashley" };
         String[] students1B = { "Bobby", "Brian", "Bill" };
         String[] students1C = { "Clara", "Celine", "Charlie", "Caroline" };
         String[] students1D = { "Daniel", "Daisy", "Diana" };
 
-        // 1A
-        System.out.printf("-------------------------%n");
-        System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
-        System.out.printf("-------------------------%n");
-        
-        for (int i = 0; i < students1A.length; i++) {
-            System.out.printf("| %-3d | %-15s |%n", (i + 1), students1A[i]);
-        }
-
-        System.out.printf("-------------------------%n%n");
-        
-        // 1B
-        System.out.printf("-------------------------%n");
-        System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
-        System.out.printf("-------------------------%n");
-        
-        for (int i = 0; i < students1B.length; i++) {
-            System.out.printf("| %-3d | %-15s |%n", (i + 1), students1B[i]);
-        }
-
-        System.out.printf("-------------------------%n%n");
-
-        // 1C
-        System.out.printf("-------------------------%n");
-        System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
-        System.out.printf("-------------------------%n");
-        
-        for (int i = 0; i < students1C.length; i++) {
-            System.out.printf("| %-3d | %-15s |%n", (i + 1), students1C[i]);
-        }
-
-        System.out.printf("-------------------------%n%n");
-
-        // 1D
-        System.out.printf("-------------------------%n");
-        System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
-        System.out.printf("-------------------------%n");
-        
-        for (int i = 0; i < students1D.length; i++) {
-            System.out.printf("| %-3d | %-15s |%n", (i + 1), students1D[i]);
-        }
-
-        System.out.printf("-------------------------%n%n");
+        display(students1A);
+        display(students1B);
+        display(students1C);
+        display(students1D);
     }
 
 }
