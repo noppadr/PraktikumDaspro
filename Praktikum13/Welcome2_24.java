df6ed6d306afa1374269c75acf3d56489a16250a
Nova Diana <nopadr12@gmail.com>
2023-12-03 10:28:57 +0700

    coba pertama

--- Praktikum13/Welcome2_24.java
+++ Praktikum13/Welcome2_24.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class Welcome2_24 {
-    Scanner sc = new Scanner(System.in);
     
-    public static void displayWelcomeMessage(String name){
-        System.out.println("Selamat datang di Politeknik Negeri Malang, " + name);
-    }
-    public static void main(String[] args) {
-        displayWelcomeMessage(null);
+        Scanner sc = new Scanner (System.in);
+
+        public static void displayWelcomeMessage(String name) {
+            System.out.println("Selamat datang di Politeknik Negeri Malang, " + name);
+        }
+        public static void main(String[] args) {
+            displayWelcomeMessage(null);
     }
 }
-
