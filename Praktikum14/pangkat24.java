decfbbc8b0ffecdf6f51920e845f7f3b8ddfed39
Nova Diana <nopadr12@gmail.com>
2023-12-06 11:23:09 +0700

    fungsi rekursif

---
+++ Praktikum14/pangkat24.java
@@ -0,0 +1,23 @@
+public class pangkat24 {
+    public static int pangkat (int x, int y) {
+        if (y == 0) {
+            return 1;
+        } else {
+            return x * pangkat(x, y - 1);
+        }
+    }
+
+    public static int pangkatIterative(int x, int y) {
+        int hasil = 1;
+
+        for (int i = 1; i <= y; i++) {
+            hasil = hasil * x;
+        }
+
+        return hasil;
+    }
+    public static void main(String[] args) {
+        System.out.println(pangkat(6, 4));
+        System.out.println(pangkatIterative(6, 4));
+    }
+}
